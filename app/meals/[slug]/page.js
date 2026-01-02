ee06f2b10a2371785318c625aa97aaae0d788e55
Jaydev Ingawale <jaydevingawale@gmail.com>
2026-01-02 12:29:18 +0400

    Adding error js, notfound js and using them in multiple different scenarios

--- app/meals/[slug]/page.js
+++
@@ -1,8 +0,0 @@
-export default function MealDetails({ params }) {
-  return (
-    <main>
-      <h1>Meal Details</h1>
-      <p>{params.slug}</p>
-    </main>
-  );
-}
--- app/meals/page.js
+++ app/meals/page.js
@@ -1,26 +1,33 @@
 import Link from "next/link";
-import classess from "./page.module.css";
+import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
 import { getMeals } from "@/lib/meals";
+import { Suspense } from "react";
 
-export default async function MealsPage() {
+async function Meals() {
   const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+}
+
+export default function MealsPage() {
   return (
     <>
-      <header className={classess.header}>
+      <header className={classes.header}>
         <h1>
           Delicious meals, created
-          <span className={classess.highlight}>by you</span>
+          <span className={classes.highlight}>by you</span>
         </h1>
         <p>
           Choose your favourite recipe and cook it yourself. It is easy and fun!
         </p>
-        <p className={classess.cta}>
+        <p className={classes.cta}>
           <Link href="/meals/share">Share Your Favourite Recipe</Link>
         </p>
       </header>
-      <main className={classess.main}>
-        <MealsGrid meals={meals} />
+      <main className={classes.main}>
+        <Suspense fallback={<p className={classes.loading}>Fetching meals</p>}>
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
